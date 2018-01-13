0786a282f6507003f663c4a39a9719d2b45bc112
wangzhiwei8 <wangzhiwei8@jd.com>
2018-01-13 23:05:05 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+AppRegistry.registerComponent('threed', () => App);
+
+AppRegistry.runApplication('threed',{
+	rootTag:document.getElementById('root')
+})
