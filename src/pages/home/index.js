787c83191453b8f0eba8dbc0780cd9fd8518c7f7
wangzhiwei8 <wangzhiwei8@jd.com>
2018-01-15 14:32:04 +0800

    step4-2

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,8 @@
 
 import demoText from '../demo1/demoText';
 import demoView from '../demo1/demoView';
-export default demoView;
+import demoView1 from '../demo1/demoView1';
+export default demoView1;
 
 
 
