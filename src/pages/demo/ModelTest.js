656b3f5c54ea2c5ade511f0fb1fb3b107f382330
wangzhiwei8 <wangzhiwei8@jd.com>
2018-01-15 14:36:11 +0800

    step4-3

--- src/pages/demo/ModelTest.js
+++ src/pages/demo/ModelTest.js
@@ -17,7 +17,7 @@ import {
 } from 'react-native';
 
 const {width, height, scale} = Dimensions.get("window");
-import styles from './style';
+// import styles from './style';
 
 export default class App extends Component<{}> {
 
@@ -139,19 +139,90 @@ export default class App extends Component<{}> {
   }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+const styles = StyleSheet.create({
+    "container2": {
+        "paddingTop": 10,
+        "alignItems": "center"
+    },
+    "btn": {
+        "paddingTop": 12,
+        "paddingRight": 10,
+        "paddingBottom": 12,
+        "paddingLeft": 10,
+        "width": 300,
+        "backgroundColor": "red",
+        "justifyContent": "center",
+        "alignItems": "center",
+        "borderRadius": 18,
+        "position": "relative",
+        "zIndex": 101,
+        "marginTop":10,
+    },
+    "btnColor": {
+        "color": "#fff"
+    },
+    "panel": {
+        "position": "absolute",
+        "backgroundColor": "blue",
+        "height":height,
+        "left": 0,
+        "top": 0,
+        "bottom": 0,
+        "right": 0,
+        "zIndex": 100,
+        "justifyContent": "center",
+        "alignItems": "center"
+    },
+    "box":{
+      "justifyContent": "center",
+      "alignItems": "center",
+      "width":300,
+      "height":300,
+      "backgroundColor":"#fff"
+    },
+    "navTop":{
+      "marginTop":60,
+    },
+    "container": {
+      "flex": 1,
+
+    },
+    "innerContainer": {
+      
+      "alignItems": 'center',
+      
+    },
+    "addBottom":{
+      "position":"absolute",
+      
+      "left":0,
+      "right":0,
+      "bottom":0
+    },
+    "timeBoxWrap":{
+      "width":width,
+      "paddingLeft":20,
+      "paddingRight":20,
+      "paddingBottom":20,
+      "backgroundColor":"#fff",
+    },
+    "timeBox":{
+      "height": 120,
+      "marginTop":40,
+
+      "backgroundColor":"blue",
+      "alignItems":"center",
+      "justifyContent":"center"
+    },
+    "TextWrap":{
+      "marginTop":10,
+      "marginBottom":10
+    },
+    "alignItems":{
+      "alignItems":"center"
+    }
+
+});
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,8 @@
 import demoText from '../demo1/demoText';
 import demoView from '../demo1/demoView';
 import demoView1 from '../demo1/demoView1';
-export default demoView1;
+import ModelTest from '../demo/ModelTest';
+export default ModelTest;
 
 
 
