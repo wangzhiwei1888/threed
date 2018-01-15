2dbede2acc214d600db271be414dcc7d51671577
wangzhiwei8 <wangzhiwei8@jd.com>
2018-01-15 10:12:15 +0800

    step2

---
+++ src/1-components/ActivityIndicator/ActivityIndicatorScreen.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-sort-props */
+
+/**
+ * @flow
+ */
+
+import PropAnimating from './examples/PropAnimating';
+
+import React from 'react';
+
+const ActivityIndicatorScreen = () => (
+  <PropAnimating />
+);
+
+// storiesOf('Components', module).add('ActivityIndicator', ActivityIndicatorScreen);
+
+export default ActivityIndicatorScreen;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './App';
+import App from './1-components/ActivityIndicator/ActivityIndicatorScreen';
 AppRegistry.registerComponent('threed', () => App);
 
 AppRegistry.runApplication('threed',{
